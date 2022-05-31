886030adf3bf4b4e6835f43725306173cc8bc4fc
Agustin Ricardo <agustinricardo14@gmail.com>
2022-05-31 20:03:56 -0300

    styles login

--- src/components/login/LoginScreen.js
+++ src/components/login/LoginScreen.js
@@ -1,7 +1,9 @@
 import React, { useContext } from 'react'
 import { useNavigate } from 'react-router-dom';
 import { AuthContext } from '../../auth/authContext';
+import { useForm } from '../../hooks/useForm';
 import { types } from '../../types/types';
+import '../../styles/loginScreen.css'
 
 export const LoginScreen = () => {
   const {dispatch} =  useContext(AuthContext)
@@ -10,15 +12,22 @@ export const LoginScreen = () => {
   // Se utiliza replace en true para no poder volver para atras con la flechita
   const navigate = useNavigate();
 
-  const handleLogin = () => {
+  const [formValue, handleInputChange] = useForm();
+  const {name} = formValue;
+  const handleLogin = (e) => {
+    e.preventDefault();
     // Disparamos la action
     // Buscamos en types la accion login ya creada
     // El payload es el cambio que debe hacerse, en este caso, hay que mandar el nombre del usuario para que lo detecte y ponga el logged en true
-    const action = {
-      type: types.login,
-      payload: {name: 'Agustin'}
+    if (name) {
+      const action = {
+        type: types.login,
+        payload: {name: name}
+      }
+      dispatch(action);
+    }else {
+      alert('Ingrese un nombre');
     }
-    dispatch(action);
 
     const lastPath = localStorage.getItem('lastPath') || '/marvel'
     navigate(lastPath, {
@@ -27,9 +36,23 @@ export const LoginScreen = () => {
   }
   return (
     <div className='container mt-5'>
-      <h1>Login Screen</h1>
-      <hr />
-      <button className='btn btn-primary' onClick={handleLogin}>Login</button>
+      <form action="" onSubmit={handleLogin} className='lgS_form-container bg-dark'>
+        <h1>Login</h1>
+        <div className="lgS__form-input">
+          <label style={{padding: "5px"}}>Name: </label>
+          <input type="text" maxLength='12' name='name' value={name} onChange={handleInputChange} autoComplete="off" />
+        </div>
+        <input className='btn btn-primary btn-login' type="submit" value="Login"/>
+        <div className="icons">
+          <span>Created by:</span>
+          <a href="https://github.com/agustinricardo1" target="_blank" rel="noopener noreferrer">
+            <i class="fa-brands fa-github"></i>
+          </a>
+          <a href="https://www.linkedin.com/in/agustin-ricardo" target="_blank">
+            <i class="fa-brands fa-linkedin"></i>
+          </a>
+        </div>
+      </form>
     </div>
   )
 }
